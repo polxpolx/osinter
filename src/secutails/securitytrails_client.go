7bdcac847ebed3634c61066994b6fb4f698fc2ad
Polxpolx <security@secureworld.com>
2020-11-28 17:02:33 +0100

    Started working on security trails function.

---
+++ src/secutails/securitytrails_client.go
@@ -0,0 +1,38 @@
+package secutrails
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func clientSecuTrails(URL string, HttpMethod string, ApiKey string, json []byte) []byte {
+	client := &http.Client{}
+
+	req, err := http.NewRequest(HttpMethod, URL, bytes.NewBuffer(json))
+
+	Checkerr(err)
+	log.Printf("%s", json)
+	req.Header.Add("APIKEY", ApiKey)
+
+	resp, err := client.Do(req)
+	Checkerr(err)
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Something went wrong: ", resp.StatusCode)
+		bodybyte, err := ioutil.ReadAll(resp.Body)
+		Checkerr(err)
+		log.Printf("%s", bodybyte)
+		log.Fatal("Fatal error !")
+	}
+
+}
+
+func Checkerr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ src/secutails/securitytrails_model.go
@@ -0,0 +1,5 @@
+package secutrails
+
+const (
+	url_secutrails_ping string = "https://api.securitytrails.com/v1/ping"
+)
